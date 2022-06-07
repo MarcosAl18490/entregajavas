ecceab8079856b2dcce9e04225846dc6e47ad9cc
marcosal <marcos90.alonso90@gmail.com>
2022-06-07 18:44:41 -0300

    Entrega del proyecto final

--- codigo.js
+++ codigo.js
@@ -1,4 +1,5 @@
 // Mediante el siguiente formulario el cliente realiza su reserva para el cuarto de un hotel familiar.
+// Los datos personales serán almacenados en la session Storage
 
 class datos{
     constructor(nacionalidad,edad,profesion){
@@ -10,7 +11,8 @@ class datos{
 
 
 let titulo = document.getElementById("encabezado");
-titulo.innerHTML = "<h1 style='color:red; font-family:helvetica;'>Bienvenidos al Resort Altos de Simic</h1>";
+titulo.innerHTML = "<h1 style='color:red; font-family:helvetica;'>Bienvenido al sistema de reservas del resort 'Altos de Simic'</h1>";
+
 
 let persona1 = new datos ();
 let fechaelegida = "";
@@ -38,16 +40,21 @@ let profesion = document.getElementById("valor6");
 
 // Los siguientes tres campos apareceran en forma de objeto previamente creado
 nacionalidad.onchange = () => { persona1.nacionalidad = nacionalidad.value};
+    
 edad.onchange = () => { persona1.edad = edad.value};
-profesion.onchange = () => { 
-    persona1.profesion = profesion.value;
-    console.log(persona1)
+    
+profesion.onchange = () => { persona1.profesion = profesion.value;
+    
+    console.log(persona1);
+    sessionStorage.setItem("Nacionalidad", nacionalidad.value);
+    sessionStorage.setItem("Edad", edad.value);
+    sessionStorage.setItem("Profesión", profesion.value);
 };
 
 
 function elegirfecha(fecha) {
     fechaelegida = fecha.value;
-    console.log(fecha.value)    
+    console.log("La fecha elegida es: " + fecha.value)    
 }
 
 function elegiractividad(actividad){
@@ -56,22 +63,27 @@ function elegiractividad(actividad){
 
 }
 
-// En la siguiente funcion a la opcion elegida por el usuario le concateno otras opciones ya establecidas
+// En la siguiente función, a la opción elegida por el usuario le concateno otras opciones ya establecidas.
 function elegirhabitacion(habitacion){
     habitacionelegida = habitacion.value;
     console.log("La Habitacion elegida es " + (habitacion.value).concat(extras));
 
 }
 
+function aceptarTerminos(terminos){
+    terminoaceptado = terminos.value;
+    console.log("El usuario aceptó los términos y condiciones")
+}
+
 function validarFormulario(event){
     event.preventDefault();
-    if (nombre.value.length>0 && apellido.value.length>0 && email.value.length>0 ){
+    if (nombre.value.length>0 && apellido.value.length>0 && email.value.length>0 && (inlineRadio1.checked || inlineRadio2.checked || inlineRadio3.checked) ){
         for (let i = 3; i <= 3; i--) {
-            console.log("Confirmando su reserva, espere " + i + "seg");
+            alert("Confirmando su reserva, espere " + i + "seg");
             if (i === 1) {
                 break;
             }
-            enviarDatos;
+           
         }
             Swal.fire({
                 icon: 'success',
@@ -79,24 +91,25 @@ function validarFormulario(event){
                 text: 'Su reserva a sido confirmada',
                 footer: 'Gracias por confiar en nosotros',
           })
-        
+          enviarDatos();
     }
     else{
         Swal.fire({
             icon: 'error',
             title: 'Atención',
-            text: 'Debes completar los campos requeridos',
+            text: 'Complete los campos obligatorios',
           })
         
     }
+  
 }
 
 function enviarDatos(){
  fetch('https://jsonplaceholder.typicode.com/posts', {
   method: 'POST',
   body: JSON.stringify({
-    title: 'Caracteristicas de la reserva',
-    body: "Nombre" + nombre + "Apellido:" + apellido,
+    title: 'Datos del cliente',
+    body: "Nombre: " + nombre.value + ", Apellido: " + apellido.value + ", Nacionalidad: " + nacionalidad.value + ", Edad: " + edad.value + ", Profesión: " + profesion.value,
     userId: 1,
   }),
   headers: {
